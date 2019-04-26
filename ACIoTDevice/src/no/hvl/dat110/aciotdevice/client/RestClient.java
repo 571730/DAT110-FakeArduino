e65a3c740c211444973ff7703bc3dbb559c7699c
Mikkel Espolin Birkeland <mikkel@158-37-230-150.hvl.no>
2019-04-26 13:32:42 +0200

    Done?

--- ACIoTDevice/src/no/hvl/dat110/aciotdevice/client/RestClient.java
+++ ACIoTDevice/src/no/hvl/dat110/aciotdevice/client/RestClient.java
@@ -1,5 +1,12 @@
 package no.hvl.dat110.aciotdevice.client;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
 public class RestClient {
 
 	public RestClient() {
@@ -9,9 +16,56 @@ public class RestClient {
 	private static String logpath = "/accessdevice/log";
 
 	public void doPostAccessEntry(String message) {
-
 		// TODO: implement a HTTP POST on the service to post the message
-		
+		try (Socket s = new Socket(Configuration.host, Configuration.port)) {
+			// construct the GET request
+			String postMessage = "{\n   \"message\": \"" + message + "\"\n}";
+			String httppostrequest = "POST " + logpath + " HTTP/1.1\r\n"
+					+ "Accept: application/json\r\n"
+					+ "Content-length: " + postMessage.length() + "\r\n"
+					+ "Host: localhost\r\n"
+					+ "Connection: close\r\n"
+					+ "\r\n"
+					+ postMessage
+					+ "\r\n";
+
+			System.out.println(httppostrequest);
+			// sent the HTTP request
+			OutputStream output = s.getOutputStream();
+
+			PrintWriter pw = new PrintWriter(output, false);
+
+			pw.print(httppostrequest);
+			pw.flush();
+
+			// read the HTTP response
+			InputStream in = s.getInputStream();
+
+			Scanner scan = new Scanner(in);
+			StringBuilder jsonresponse = new StringBuilder();
+			boolean header = true;
+
+			while (scan.hasNext()) {
+				String nextline = scan.nextLine();
+				if (header) {
+					System.out.println(nextline);
+				} else {
+					jsonresponse.append(nextline);
+				}
+				// simplified approach to identifying start of body: the empty line
+				if (nextline.isEmpty()) {
+					header = false;
+				}
+			}
+			System.out.println("BODY:");
+			System.out.println(jsonresponse.toString());
+
+			scan.close();
+
+		} catch (IOException ex) {
+			System.err.println(ex);
+		}
+
 	}
 	
 	private static String codepath = "/accessdevice/code";
@@ -21,6 +75,53 @@ public class RestClient {
 		AccessCode code = null;
 		
 		// TODO: implement a HTTP GET on the service to get current access code
+		try (Socket s = new Socket(Configuration.host, Configuration.port)) {
+
+			// construct the GET request
+			String httpgetrequest = "GET " + codepath + " HTTP/1.1\r\n" + "Accept: application/json\r\n"
+					+ "Host: localhost\r\n" + "Connection: close\r\n" + "\r\n";
+
+			// sent the HTTP request
+			OutputStream output = s.getOutputStream();
+
+			PrintWriter pw = new PrintWriter(output, false);
+
+			pw.print(httpgetrequest);
+			pw.flush();
+
+			// read the HTTP response
+			InputStream in = s.getInputStream();
+			Scanner scan = new Scanner(in);
+			StringBuilder jsonresponse = new StringBuilder();
+			boolean header = true;
+
+			while (scan.hasNext()) {
+				String nextline = scan.nextLine();
+				if (header) {
+					System.out.println(nextline);
+				} else {
+					jsonresponse.append(nextline);
+				}
+				// simplified approach to identifying start of body: the empty line
+				if (nextline.isEmpty()) {
+					header = false;
+				}
+			}
+			System.out.println("BODY:");
+			System.out.println(jsonresponse.toString());
+			String[] codeStringList = jsonresponse.toString().replaceAll("[^0-9,]", "")
+					.split(",");
+			int[] codeInt = new int[codeStringList.length];
+			for (int i = 0; i < codeInt.length; i++){
+				codeInt[i] = Integer.parseInt(codeStringList[i]);
+			}
+			code = new AccessCode();
+			code.setAccesscode(codeInt);
+			scan.close();
+
+		} catch (IOException ex) {
+			System.err.println(ex);
+		}
 		
 		return code;
 	}
